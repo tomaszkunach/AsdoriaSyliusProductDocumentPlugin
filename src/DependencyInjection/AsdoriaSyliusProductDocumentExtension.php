19872e74414c83c9b577667515465f7b9a1a939b
tomasz.kunach <tomasz.kunach@nfq.com>
2025-02-06 12:07:20 +0100

    added migrations config to AsdoriaSyliusProductDocumentExtension

--- src/DependencyInjection/AsdoriaSyliusProductDocumentExtension.php
+++ src/DependencyInjection/AsdoriaSyliusProductDocumentExtension.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 namespace Asdoria\SyliusProductDocumentPlugin\DependencyInjection;
 
 use Asdoria\SyliusProductDocumentPlugin\Uploader\Uploader;
+use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Sylius\Component\Core\Filesystem\Adapter\FilesystemAdapterInterface;
 use Sylius\Component\Core\Filesystem\Adapter\FlysystemFilesystemAdapter;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
@@ -17,8 +20,9 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
  *
  * @author  Hugo Duval <hugo.duval@asdoria.com>
  */
-class AsdoriaSyliusProductDocumentExtension extends AbstractResourceExtension
+class AsdoriaSyliusProductDocumentExtension extends AbstractResourceExtension implements PrependExtensionInterface, ExtensionInterface
 {
+    use PrependDoctrineMigrationsTrait;
     /**
      * @param array            $configs
      * @param ContainerBuilder $container
@@ -44,5 +48,37 @@ class AsdoriaSyliusProductDocumentExtension extends AbstractResourceExtension
             },
         );
         $loader->load('services.yaml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependDoctrineMigrations($container);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getMigrationsNamespace(): string
+    {
+        return 'Asdoria\SyliusProductDocumentPlugin\Migrations';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getMigrationsDirectory(): string
+    {
+        return '@SyliusProductDocumentPlugin/Migrations';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getNamespacesOfMigrationsExecutedBefore(): array
+    {
+        return ['Sylius\Bundle\CoreBundle\Migrations'];
     }
 }
